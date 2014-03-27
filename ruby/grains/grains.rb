0b2d93913e5dd13966a095719f6230bc21d821fa
Jacob Hooey <jhooey@gmail.com>
2014-03-27 11:48:56 -0400

    changed the class so that it creates an array of results instead of calculating them every time

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,5 +1,15 @@
 class Grains
+  
+  def initialize
+    @squares = *(1..64)
+    @squares.map! { |a| 2**(a-1) }
+  end
+  
   def square(x)
-    2 ** (x-1)
+    @squares[x-1]
+  end
+  
+  def self.calculate_total
+    0
   end
 end
