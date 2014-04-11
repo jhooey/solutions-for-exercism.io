d6ec10425909128f5c4ab3e706f5c91bcc8a5901
Jacob Hooey <jhooey@gmail.com>
2014-04-11 19:13:19 +0000

    passed 4 of 6 tests, first iteration

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,5 +1,9 @@
 class Anagram
   def initialize(word)
-      
+    @anagrams = word.chars.to_a.permutation.map(&:join)  
+  end
+  
+  def match(options)
+    options.each_with_object([]){|option, matches| if @anagrams.include? option then matches << option end }
   end
 end
