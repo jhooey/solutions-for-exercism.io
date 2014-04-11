112c19d38f11ac557abf3a37cbd31571ebaf6f57
Jacob Hooey <jhooey@gmail.com>
2014-04-11 19:27:51 +0000

    passed the last 2, added downcase and delete

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,9 +1,10 @@
 class Anagram
   def initialize(word)
-    @anagrams = word.chars.to_a.permutation.map(&:join)  
+    @anagrams = word.downcase.chars.to_a.permutation.map(&:join)
+    @anagrams.delete(word) #remove original word
   end
   
   def match(options)
-    options.each_with_object([]){|option, matches| if @anagrams.include? option then matches << option end }
+    options.each_with_object([]){|option, matches| if @anagrams.include? option.downcase then matches << option end }
   end
 end
