c238a00e54bc017c4036b9213099473284fc022f
Jacob Hooey <jhooey@gmail.com>
2014-05-08 10:57:14 -0700

    passed the last 4 tests

--- ruby/grade-school/school.rb
+++ ruby/grade-school/school.rb
@@ -3,13 +3,20 @@ class School
     @registry = {}
   end
   def to_hash
-    @registry
+    Hash[@registry.sort]
   end
   def add (name, grade)
     if @registry.has_key?(grade)
       @registry[grade].push(name)
+      @registry[grade] = @registry[grade].sort
     else
       @registry[grade] = [name]
     end
+  end
+  def grade (grade)
+    if @registry[grade] 
+      return @registry[grade]
+    end
+    []
   end
 end
