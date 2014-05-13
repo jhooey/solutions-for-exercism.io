9ac2a0452cb1930579907f7a1908e50ecc957964
Jacob Hooey <jhooey@gmail.com>
2014-05-13 11:15:53 -0700

    implemented each_with_object to reduce total # of lines of code

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,12 +1,9 @@
 class ETL 
   def self.transform(old_hash)
-    new_hash = {}
-    old_hash.each do |key, values|
-      values.each do |value|
-        new_hash.store(value.downcase, key)
-      end
-    end
-    
-    new_hash
+    old_hash.each_with_object(Hash.new(0)) {
+      |(key, values), new_hash| values.each {
+        |value| new_hash.store(value.downcase, key)
+      }
+    }
   end
 end
